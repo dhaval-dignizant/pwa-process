9487473b6742638ba608f17eb2ab5e05164bfc56
Dhaval <dhaval.chotaliya@dignizant.com>
2019-10-07 16:27:24 +0530

    Changes

---
+++ precache-manifest.8cba6e3657bfc2a897eb48996e4892e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "00e470d8e9fd106f1efe",
+    "url": "/js/chunk-vendors.bcbdbe80.js"
+  },
+  {
+    "revision": "63f273a99c0ed5143b99",
+    "url": "/js/app.07d90edd.js"
+  },
+  {
+    "revision": "0062728fb7d7e1e52bfb9b93b09d4c65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "63f273a99c0ed5143b99",
+    "url": "/css/app.578ae515.css"
+  }
+];
